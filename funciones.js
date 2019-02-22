0b084bc7050e0d0076f7334f0ae19daff8b9826a
jonberge1337 <jonbergerandi@gmail.com>
2019-02-22 10:25:11 +0100

    nos falta las validaciones de javascript

--- funciones.js
+++ funciones.js
@@ -30,6 +30,37 @@ function inicializarEventos(e) {
 
     var contacto = document.getElementById("privacidad");
     contacto.addEventListener("click", ocultar, false);
+
+    var nombre = document.getElementById("apellidosynombre");
+    nombre.addEventListener("blur", verificarNombre, false);
+    nombre.addEventListener("focus", reiniciarNombre, false);
+
+    var dni = document.getElementById("dni");
+    dni.addEventListener("blur", verificarDNI, false);
+    dni.addEventListener("focus", reiniciarDNI, false);
+
+    var email = document.getElementById("email");
+    email.addEventListener("blur", verificarEmail, false);
+    email.addEventListener("focus", reiniciarEmail, false);
+
+    var direccion = document.getElementById("direccion");
+    direccion.addEventListener("blur", verificarDireccion, false);
+    direccion.addEventListener("focus", reiniciarDireccion, false);
+
+    var poblacion = document.getElementById("poblacion");
+    poblacion.addEventListener("blur", verificarPoblacion, false);
+    poblacion.addEventListener("focus", reiniciarPoblacion, false);
+
+    var cp = document.getElementById("cp");
+    cp.addEventListener("blur", verificarCp, false);
+    cp.addEventListener("focus", reiniciarCp, false);
+
+    var card = document.getElementById("card");
+    card.addEventListener("blur", verificarCard, false);
+    card.addEventListener("focus", reiniciarCard, false);
+
+    var boton = document.getElementById("enviar");
+    boton.addEventListener("click", renovar, false);
 }
 
 function buscar(e) {
@@ -46,25 +77,6 @@ function ocultar(e) {
     // e.target es otra propiedad del evento, para saber el botón que se ha pulsado
     var boton = e.target;
     var array = ["subInicio", "subConvenios", "subColegiacion", "subContratos", "subFormacion", "subBolsa", "subCookies", "subPrivacidad", "subContacto"];
-    // if (boton.id == "Inicio") {
-    // document.getElementById("subConvenios").style.display = "none";
-    // document.getElementById("subColegiacion").style.display = "none";
-    // document.getElementById("subContratos").style.display = "none";
-    // document.getElementById("subFormacion").style.display = "none";
-    // document.getElementById("subBolsa").style.display = "none";
-    // document.getElementById("subCookies").style.display = "none";
-    // document.getElementById("subPrivacidad").style.display = "none";
-    // document.getElementById("subInicio").style.display = "inline";
-    // }else if (boton.id == "Convenios") {
-    //     document.getElementById("subInicio").style.display = "none";
-    //     document.getElementById("subColegiacion").style.display = "none";
-    //     document.getElementById("subContratos").style.display = "none";
-    //     document.getElementById("subFormacion").style.display = "none";
-    //     document.getElementById("subBolsa").style.display = "none";
-    //     document.getElementById("subCookies").style.display = "none";
-    //     document.getElementById("subPrivacidad").style.display = "none";
-    //     document.getElementById("subConvenios").style.display = "inline";
-    // }
 
     if (boton.id == "inicio") {
         for (var i = 0; i < array.length; i++) {
@@ -72,53 +84,180 @@ function ocultar(e) {
         }
         document.getElementById("subInicio").style.display = "inline";
 
-    }else if (boton.id =="convenios") {
+    } else if (boton.id == "convenios") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subConvenios").style.display = "inline";
 
-    }else if (boton.id == "colegiacion") {
+    } else if (boton.id == "colegiacion") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subColegiacion").style.display = "inline";
 
-    }else if (boton.id == "contratos") {
+    } else if (boton.id == "contratos") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subContratos").style.display = "inline";
 
-    }else if (boton.id == "formacion") {
+    } else if (boton.id == "formacion") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subFormacion").style.display = "inline";
 
-    }else if (boton.id == "bolsa") {
+    } else if (boton.id == "bolsa") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subBolsa").style.display = "inline";
-        
-    }else if (boton.id == "contacto") {
+
+    } else if (boton.id == "contacto") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subContacto").style.display = "inline";
 
-    }else if (boton.id == "cookies") {
+    } else if (boton.id == "cookies") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subCookies").style.display = "inline";
 
-    }else if (boton.id == "privacidad") {
+    } else if (boton.id == "privacidad") {
         for (var i = 0; i < array.length; i++) {
             document.getElementById(array[i]).style.display = "none";
         }
         document.getElementById("subPrivacidad").style.display = "inline";
+    }
+}
+
+    // Formulario para Inscripción y "Contáctanos"
+    function verificarNombre() {
+        var valor = document.getElementById("apellidosynombre").value;
+        var expreg = /^[a-zñ ]+$/i;
+        if (!expreg.test(valor)) {
+            document.getElementById("errorNombre").style.display = "block";
+        }
+    }
+
+    function reiniciarNombre() {
+        var x = document.getElementById("errorNombre");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("apellidosynombre").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function verificarDNI() {
+        var valor = document.getElementById("dni").value;
+        var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+        if (!(/^\d{8}[A-Z]$/.test(valor))) {
+            document.getElementById("errorDni").style.display = "block";
+        }
+
+        if (valor.charAt(8) != letras[(valor.substring(0, 8)) % 23]) {
+            document.getElementById("errorDni").style.display = "block";
+        }
+    }
+
+    function reiniciarDNI() {
+        var x = document.getElementById("errorDni");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("dni").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function verificarEmail() {
+        var valor = document.getElementById("email").value;
+        var expreg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+        if (!expreg.test(valor)) {
+            document.getElementById("errorEmail").style.display = "block";
+        }
+
+    }
+
+    function reiniciarEmail() {
+        var x = document.getElementById("errorEmail");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("email").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function verificarDireccion() {
+        var valor = document.getElementById("direccion").value;
+        var expreg = /^[a-zñ /º 0-9]+$/i;
+        if (!expreg.test(valor)) {
+            document.getElementById("errorDireccion").style.display = "block";
+        }
+    }
+
+    function reiniciarDireccion() {
+        var x = document.getElementById("errorDireccion");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("direccion").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function verificarPoblacion() {
+        var valor = document.getElementById("poblacion").value;
+        var expreg = /^[a-zñ ]+$/i;
+        if (!expreg.test(valor)) {
+            document.getElementById("errorPoblacion").style.display = "block";
+        }
+    }
+
+    function reiniciarPoblacion() {
+        var x = document.getElementById("errorPoblacion");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("poblacion").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function verificarCp() {
+        var valor = document.getElementById("cp").value;
+        var expreg = /^\d{5}$/;
+        if (!expreg.test(valor)) {
+            document.getElementById("errorCp").style.display = "block";
+        }
+    }
+
+    function reiniciarCp() {
+        var x = document.getElementById("errorCp");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("cp").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function verificarCard() {
+        var valor = document.getElementById("card").value;
+        var expreg = /^\d{4}[ -]{0,1}\d{4}[ -]{0,1}\d{4}[ -]{0,1}\d{4}$/;
+        if (!expreg.test(valor)) {
+            document.getElementById("errorCard").style.display = "block";
+        }
+    }
+
+    function reiniciarCard() {
+        var x = document.getElementById("errorCard");
+        if (x.style.display == "block") {
+            var valor = document.getElementById("card").value = "";
+            x.style.display = "none";
+        }
+    }
+
+    function renovar(e) {
+        var x = document.getElementById("principal");
+        var nombre = document.getElementById("apellidosynombre").value;
+        x.style.visibility = "hidden"
+        document.write("<h1>PedidoEnviado:</h1> " + nombre);
     }
 
     // if(boton.id.localeCompare("Convenios")){
@@ -203,7 +342,6 @@ function ocultar(e) {
     // }else if(boton.id == "Contacto"){
 
     // }
-}
 
 
 // window.addEventListener('load',inicializarEventos,false);
